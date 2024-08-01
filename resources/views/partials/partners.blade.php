832b6a6bd39271c8b5f416fae0cb9080a8a3780d
Dohd <alloicebrian@gmail.com>
2024-08-01 11:13:15 +0300

    Add sample image for partners

--- resources/views/partials/partners.blade.php
+++ resources/views/partials/partners.blade.php
@@ -1,167 +1,88 @@
 <section id="pricing" class="pricing section">
+  <!-- Section Title -->
+  <div class="container section-title" data-aos="fade-up">
+    <h2>Partners</h2>
+    <p>Our Partners</p>
+  </div><!-- End Section Title -->
 
-    <!-- Section Title -->
-    <div class="container section-title" data-aos="fade-up">
-      <h2>Partners</h2>
-      <p>Our Partners</p>
-    </div><!-- End Section Title -->
+  <div class="container">
 
-    <div class="container">
+    <div class="row gy-3">
 
-      <div class="row gy-3">
-
-        <div class="col-xl-3 col-lg-6" data-aos="fade-up" data-aos-delay="100">
-          <div class="pricing-item">
-            <h3>Government of Kenya</h3>
-            <h4><span> Image </span></h4>
-            <!-- <h4><sup>$</sup>0<span> / month</span></h4> -->
-            <!-- <ul>
-              <li>Aida dere</li>
-              <li>Nec feugiat nisl</li>
-              <li>Nulla at volutpat dola</li>
-              <li class="na">Pharetra massa</li>
-              <li class="na">Massa ultricies mi</li>
-            </ul>
-            <div class="btn-wrap">
-              <a href="#" class="btn-buy">Buy Now</a>
-            </div> -->
-
-          </div>
-        </div><!-- End Pricing Item -->
-
-        <div class="col-xl-3 col-lg-6" data-aos="fade-up" data-aos-delay="200">
-          <div class="pricing-item featured-">
-            <h3>Inclusion Africa</h3>
-            <h4><span>Image</span></h4>
-            <!-- <ul>
-              <li>Aida dere</li>
-              <li>Nec feugiat nisl</li>
-              <li>Nulla at volutpat dola</li>
-              <li>Pharetra massa</li>
-              <li class="na">Massa ultricies mi</li>
-            </ul>
-            <div class="btn-wrap">
-              <a href="#" class="btn-buy">Buy Now</a>
-            </div> -->
-          </div>
-        </div><!-- End Pricing Item -->
-
-        <div class="col-xl-3 col-lg-6" data-aos="fade-up" data-aos-delay="400">
-          <div class="pricing-item">
-            <h3>CAVAW</h3>
-            <h4><span>Image</span></h4>
-            <!-- <ul>
-              <li>Aida dere</li>
-              <li>Nec feugiat nisl</li>
-              <li>Nulla at volutpat dola</li>
-              <li>Pharetra massa</li>
-              <li>Massa ultricies mi</li>
-            </ul>
-            <div class="btn-wrap">
-              <a href="#" class="btn-buy">Buy Now</a>
-            </div> -->
-          </div>
-        </div><!-- End Pricing Item -->
-
-        <div class="col-xl-3 col-lg-6" data-aos="fade-up" data-aos-delay="400">
-          <div class="pricing-item">
-            <!-- <span class="advanced">Advanced</span> -->
-            <h3>FDUV</h3>
-            <h4><span>Image</span></h4>
-            <!-- <ul>
-              <li>Aida dere</li>
-              <li>Nec feugiat nisl</li>
-              <li>Nulla at volutpat dola</li>
-              <li>Pharetra massa</li>
-              <li>Massa ultricies mi</li>
-            </ul>
-            <div class="btn-wrap">
-              <a href="#" class="btn-buy">Buy Now</a>
-            </div> -->
+      <div class="col-xl-3 col-lg-6" data-aos="fade-up" data-aos-delay="100">
+        <div class="pricing-item">
+          <div class="mx-auto" style="width: 200px; height: 200px">
+            <img src="{{ asset('img/partners/gok.png') }}" alt="GOK" style='height: 100%; width: 100%; object-fit:fill'>
           </div>
         </div>
-        
-        <!-- End Pricing Item -->
+      </div><!-- End Pricing Item -->
 
-      </div>
-
-      <div class="row gy-3 mt-1">
-
-        <div class="col-xl-3 col-lg-6" data-aos="fade-up" data-aos-delay="100">
-          <div class="pricing-item">
-            <h3>KNCHR</h3>
-            <h4><span> Image </span></h4>
-            <!-- <h4><sup>$</sup>0<span> / month</span></h4> -->
-            <!-- <ul>
-              <li>Aida dere</li>
-              <li>Nec feugiat nisl</li>
-              <li>Nulla at volutpat dola</li>
-              <li class="na">Pharetra massa</li>
-              <li class="na">Massa ultricies mi</li>
-            </ul>
-            <div class="btn-wrap">
-              <a href="#" class="btn-buy">Buy Now</a>
-            </div> -->
-
-          </div>
-        </div><!-- End Pricing Item -->
-
-        <div class="col-xl-3 col-lg-6" data-aos="fade-up" data-aos-delay="200">
-          <div class="pricing-item featured-">
-            <h3>OSIEA</h3>
-            <h4><span>Image</span></h4>
-            <!-- <ul>
-              <li>Aida dere</li>
-              <li>Nec feugiat nisl</li>
-              <li>Nulla at volutpat dola</li>
-              <li>Pharetra massa</li>
-              <li class="na">Massa ultricies mi</li>
-            </ul>
-            <div class="btn-wrap">
-              <a href="#" class="btn-buy">Buy Now</a>
-            </div> -->
-          </div>
-        </div><!-- End Pricing Item -->
-
-        <div class="col-xl-3 col-lg-6" data-aos="fade-up" data-aos-delay="400">
-          <div class="pricing-item">
-            <h3>NCFPWD</h3>
-            <h4><span>Image</span></h4>
-            <!-- <ul>
-              <li>Aida dere</li>
-              <li>Nec feugiat nisl</li>
-              <li>Nulla at volutpat dola</li>
-              <li>Pharetra massa</li>
-              <li>Massa ultricies mi</li>
-            </ul>
-            <div class="btn-wrap">
-              <a href="#" class="btn-buy">Buy Now</a>
-            </div> -->
-          </div>
-        </div><!-- End Pricing Item -->
-
-        <div class="col-xl-3 col-lg-6" data-aos="fade-up" data-aos-delay="400">
-          <div class="pricing-item">
-            <!-- <span class="advanced">Advanced</span> -->
-            <h3>Inclusion Africa</h3>
-            <h4><span>Image</span></h4>
-            <!-- <ul>
-              <li>Aida dere</li>
-              <li>Nec feugiat nisl</li>
-              <li>Nulla at volutpat dola</li>
-              <li>Pharetra massa</li>
-              <li>Massa ultricies mi</li>
-            </ul>
-            <div class="btn-wrap">
-              <a href="#" class="btn-buy">Buy Now</a>
-            </div> -->
+      <div class="col-xl-3 col-lg-6" data-aos="fade-up" data-aos-delay="200">
+        <div class="pricing-item">
+          {{-- <h3>Inclusion Africa</h3> --}}
+          <div class="mx-auto" style="width: 200px; height: 200px">
+            <img src="{{ asset('img/partners/gok.png') }}" alt="GOK" style='height: 100%; width: 100%; object-fit:fill'>
           </div>
         </div>
-        
-        <!-- End Pricing Item -->
+      </div><!-- End Pricing Item -->
 
+      <div class="col-xl-3 col-lg-6" data-aos="fade-up" data-aos-delay="400">
+        <div class="pricing-item">
+          {{-- <h3>CAVAW</h3> --}}
+          <div class="mx-auto" style="width: 200px; height: 200px">
+            <img src="{{ asset('img/partners/gok.png') }}" alt="GOK" style='height: 100%; width: 100%; object-fit:fill'>
+          </div>
+        </div>
+      </div><!-- End Pricing Item -->
+
+      <div class="col-xl-3 col-lg-6" data-aos="fade-up" data-aos-delay="400">
+        <div class="pricing-item">
+          {{-- <h3>FDUV</h3> --}}
+          <div class="mx-auto" style="width: 200px; height: 200px">
+            <img src="{{ asset('img/partners/gok.png') }}" alt="GOK" style='height: 100%; width: 100%; object-fit:fill'>
+          </div>
+        </div>
       </div>
-
+      <!-- End Pricing Item -->
     </div>
 
-  </section>
+    <div class="row gy-3 mt-1">
+      <div class="col-xl-3 col-lg-6" data-aos="fade-up" data-aos-delay="100">
+        <div class="pricing-item">
+          {{-- <h3>KNCHR</h3> --}}
+          <div class="mx-auto" style="width: 200px; height: 200px">
+            <img src="{{ asset('img/partners/gok.png') }}" alt="GOK" style='height: 100%; width: 100%; object-fit:fill'>
+          </div>
+        </div>
+      </div><!-- End Pricing Item -->
+
+      <div class="col-xl-3 col-lg-6" data-aos="fade-up" data-aos-delay="200">
+        <div class="pricing-item featured-">
+          {{-- <h3>OSIEA</h3> --}}
+          <div class="mx-auto" style="width: 200px; height: 200px">
+            <img src="{{ asset('img/partners/gok.png') }}" alt="GOK" style='height: 100%; width: 100%; object-fit:fill'>
+          </div>
+        </div>
+      </div><!-- End Pricing Item -->
+
+      <div class="col-xl-3 col-lg-6" data-aos="fade-up" data-aos-delay="400">
+        <div class="pricing-item">
+          {{-- <h3>NCFPWD</h3> --}}
+          <div class="mx-auto" style="width: 200px; height: 200px">
+            <img src="{{ asset('img/partners/gok.png') }}" alt="GOK" style='height: 100%; width: 100%; object-fit:fill'>
+          </div>
+        </div>
+      </div><!-- End Pricing Item -->
+
+      <div class="col-xl-3 col-lg-6" data-aos="fade-up" data-aos-delay="400">
+        <div class="pricing-item">
+          {{-- <h3>Inclusion Africa</h3> --}}
+          <div class="mx-auto" style="width: 200px; height: 200px">
+            <img src="{{ asset('img/partners/gok.png') }}" alt="GOK" style='height: 100%; width: 100%; object-fit:fill'>
+          </div>
+        </div>
+      </div>
+      <!-- End Pricing Item -->
+    </div>
+  </div>
+</section>
